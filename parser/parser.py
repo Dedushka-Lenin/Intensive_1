b2b197e034c11856950bbcd6516904f9bb61eb97
Igor Akmulin <igor.akmulin@gmail.com>
2024-10-26 18:53:06 +0300

    Test_26.10.24(18:53)

--- parser/parser.py
+++ parser/parser.py
@@ -1,16 +1,7 @@
 import cianparser
 
 import time
-import os
-import shutil
-
-######################################################################################
-
-def find_txt_files(directory):
-    for file in os.listdir(directory):
-        if file.endswith(".csv"):
-            shutil.move(file, f"parser/gathering_base/{file}")
-
+import csv
 
 ######################################################################################
 
@@ -21,15 +12,24 @@ room = range(1, 5)
 
 for loc in location_list:
     for i in room:
+        for j in range(1, 20):
 
-        parser = cianparser.CianParser(location=loc)
+            parser = cianparser.CianParser(location=loc)
 
-        data = parser.get_flats(
-            deal_type="sale", rooms=(i), 
-            with_saving_csv=True, 
-            additional_settings={"start_page":1, "end_page":54}
-        )
+            data = parser.get_flats(
+                deal_type="sale", rooms=(i), 
+                with_saving_csv=False,
+                with_extra_data=True,
+                additional_settings={"start_page":1, "end_page":54}
+            )
 
-        find_txt_files(os.getcwd())
+            with open(f'parser/base/base_{j}_{loc}_{i}.csv', 'w', newline='', encoding="utf-8") as file:
+                writer = csv.DictWriter(file, fieldnames=data[0].keys())
+                writer.writeheader()
 
-        time.sleep(5)
+                for dict_item in data:
+                    writer.writerow(dict_item)
+
+            time.sleep(5)
+
+# начал в 17:00

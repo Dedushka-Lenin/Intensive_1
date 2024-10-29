d4f6e67055d68f9b8681881a7ec372abfc957f47
Dedushka-Lenin <150888199+Dedushka-Lenin@users.noreply.github.com>
2024-10-29 11:48:44 +0300

    Test_29.10.24(11:48)

--- parser/unification.py
+++ parser/unification.py
@@ -9,4 +9,4 @@ all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
 
-combined_csv.to_csv( "../res.csv", index=False, encoding='utf-8-sig')
+combined_csv.to_csv( "./res.csv", index=False, encoding='utf-8-sig')

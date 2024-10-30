68e999904726abae22e200f41ebcc6a952a4aa5a
Igor Akmulin <igor.akmulin@gmail.com>
2024-10-30 13:53:57 +0300

    Test_30.10.24(13:53)

--- parser/unification.py
+++ parser/unification.py
@@ -1,12 +1,7 @@
 import pandas as pd    #  pip install pandas
-import glob
 from pathlib import Path
 
-data_dir = Path("base")
+data_dir = Path("parser/base")
 
-extension = 'csv'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-
-combined_csv.to_csv( "./res.csv", index=False, encoding='utf-8-sig')
+df = pd.concat([pd.read_csv(f) for f in data_dir.glob("*.csv")], ignore_index=True)
+df.to_csv("parser/result.csv", index=False)

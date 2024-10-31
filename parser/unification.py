821820023dd593a7d59d5546c180d14b2c943cb7
Igor Akmulin <igor.akmulin@gmail.com>
2024-11-01 02:12:08 +0300

    Test_1.11.24(2:12)

--- parser/unification.py
+++ parser/unification.py
@@ -1,7 +1,7 @@
-import pandas as pd    #  pip install pandas
+import pandas as pd
 from pathlib import Path
 
-data_dir = Path("parser/base")
+data_dir = Path("parser/base")  # Папка с csv
 
 df = pd.concat([pd.read_csv(f) for f in data_dir.glob("*.csv")], ignore_index=True)
-df.to_csv("parser/result.csv", index=False)
+df.to_csv("parser/result.csv", index=False) # Файл с результатом
